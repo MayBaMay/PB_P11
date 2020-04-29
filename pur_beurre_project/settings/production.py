7cd594920088240cf44651ac9c89d7cc12b84385
MayBaMay <maylis.baschet@gmail.com>
2020-04-29 16:23:26 +0200

    settings production reset_password by email

--- pur_beurre_project/settings/production.py
+++ pur_beurre_project/settings/production.py
@@ -13,3 +13,11 @@ sentry_sdk.init(
     # django.contrib.auth) you may enable sending PII data.
     send_default_pii=True
 )
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'purbeurre.team@gmail.com'
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = 'Pur Beurre Team <noreply@purbeurre.com>'
